ba409d3f50c0efb540316983b4d4e8120c260223
Spencer Ley <spencerley234@gmail.com>
2024-03-14 16:45:56 +0000

    create and revert scripts

--- intro_to_azure/blob_storage/create_public_storage_and_container_and_blob.sh
+++ intro_to_azure/blob_storage/create_public_storage_and_container_and_blob.sh
@@ -1,31 +1,34 @@
-# create storage account
+#!/bin/bash
 
+# create storage account
+az storage account create --name tech257spencerstorage --resource-group tech257 --location uksouth --sku Standard_LRS --allow-blob-public-access true
 
 # enable anonymous blob access on storage account
 
-
 # create container
-
+az storage container create --account-name tech257spencerstorage --name appcontainer --public-access blob --auth-mode login
 
 # download a cat picture (jpg) using the curl command
-
-
-# rename the cat pic to something like cat.xxx
-
+curl -o cat.jpg https://i.natgeofe.com/n/4cebbf38-5df4-4ed0-864a-4ebeb64d33a4/NationalGeographic_1468962_4x3.jpg
 
 # upload blob
+az storage blob upload --account-name tech257spencerstorage --container-name appcontainer --name cat.jpg --file cat.jpg --auth-mode login
 
+# make copy of index to revert back to later
+cd tech257-sparta-app/app/views
 
-# make blob public
+cp index.ejs index.ejs.backup
 
+# line 28 modify homepage file (index.ejs found in views folder) to include cat image in blob storage (could use sed command to replace )
+sed -i '/<h2>/a\ <img src="https://tech257spencerstorage.blob.core.windows.net/appcontainer/cat.jpg" alt="cat">' tech257-sparta-app/app/views/index.ejs
 
-# modify homepage file (index.ejs found in views folder) to include cat image in blob storage (could use sed command to replace )
-
+# app location
+cd ../../../home/adminuser/tech257-sparta-app/app
 
 # use pm2 to start the app
-
+pm2 stop app
 
 # start the app using pm2
-
+pm2 start app.js
 
 # Test your script works
--- intro_to_azure/blob_storage/revert_blob_script.sh
+++ intro_to_azure/blob_storage/revert_blob_script.sh
@@ -1,13 +1,24 @@
-# change homepage index.ejs file back to normal (could use sed command to do the opposite of before)
+#!/bin/bash
 
+cd tech257-sparta-app/app/views
+
+rm index.ejs
+
+cp index.ejs.backup index.ejs
+
+# app location
+cd ..
 
 # use pm2 to kill the app running
-
+pm2 stop app
 
 # use pm2 to start the app
-
+pm2 start app.js
 
 # remove storage account you created in the first script
-
+az storage account delete -n tech257spencerstorage -g tech257
 
 # Test your script works
+
+
+
